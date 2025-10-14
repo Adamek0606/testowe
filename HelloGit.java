59d3c17daa71823efc3d3ed35b7653072547c9e3
Krystian0606 <krystiank633@gmail.com>
2025-10-14 12:00:30 +0200

    zmiana

--- HelloGit.java
+++ HelloGit.java
@@ -1,5 +1,5 @@
-public class Main {
+public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello moje!");
+        System.out.println("Hello col!");
     }
 }
